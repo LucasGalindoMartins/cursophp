2df464d89e57d0224555c48e1657d2253fa9fcd8
Lucas Galindo Martins <165849134+LucasGalindoMartins@users.noreply.github.com>
2025-01-24 12:41:50 -0300

    Tipos primitivos em PHP

---
+++ ex003/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Tipos primitivos em PHP</title>
+    </head>
+    <body>
+        <h1>Testes de tipos primitivos em PHP</h1>
+
+        <?php 
+            // 0x = hexadecimal ; 0b = binário ; 0 =Octal
+            // $num = 0;
+            // echo "O valor da variável é $num.";
+            
+
+            // $v = 300;
+            // var_dump($v);
+
+
+            // $num = (int) 3e2; // 3 x (10^2) coerção
+            // // echo "O valor é $num";
+            // var_dump($num);
+            // TODA POTENCIA É DO TIPO FLOAT
+
+
+            // $num = (int) "950";
+            // var_dump($num);
+
+
+            // $casado = true;
+            // // var_dump($casado);
+            // print "O valor para casado é $casado";
+
+
+            // $vet = [6 ,2.5 ,"Maria", false, 5];
+            // var_dump($vet)
+
+
+            // class Pessoa
+            // {
+            //     private string $nome;
+            // }
+
+            // $p = new Pessoa;
+            // var_dump($p);
+        ?>
+    </body>
+</html>
