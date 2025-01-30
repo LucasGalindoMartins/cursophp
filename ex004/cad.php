792e4a8587c99cda58bda95d8f1bf9ede68b22af
Lucas Galindo Martins <165849134+LucasGalindoMartins@users.noreply.github.com>
2025-01-30 19:15:04 -0300

    estilização do resultado php

---
+++ ex004/cad.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Resultado</title>
+        <link rel="stylesheet" href="styles/style.css">
+    </head>
+    <body>
+        <main>
+            <section>
+                <h1>Obrigado por preencher o formulário!!</h1>
+                <p>Esses são seus dados:</p>
+                <?php
+                    $email = $_POST["email"];
+                    $senha = $_POST["senha"];
+                    echo "<p>Email: <strong>$email</strong></p>
+                    <p>Senha: <strong>$senha</strong></p>";
+                ?>
+                <a href="login.html">Voltar para página anterior</a>
+            </section>
+        </main>
+    </body>
+</html>
