b39bf98c4dfed7acd8851d6135886904adc06129
Lucas Galindo Martins <165849134+LucasGalindoMartins@users.noreply.github.com>
2025-02-02 12:54:37 -0300

    Analisador de numeros reais

---
+++ desafios/analisador/form.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Resuktado</title>
+        <link rel="stylesheet" href="styles/style.css">
+    </head>
+    <body>
+        <main>
+            <section>
+                <h1>Analisador de Número Real</h1>
+
+                <?php 
+                    $num = $_GET["num"];
+                    $full = floor($num);
+                    $fra = number_format($num - $full, 3 , ',');
+                    $number = number_format($num, 3 , ',', '.');
+
+                    echo "Analisando o número <strong>$number</strong> informado pelo usuário: <br> <br>";
+
+                    echo "- A parte inteira do número é $full <br>";
+                    echo "- A parte fracionária do número é $fra";
+                ?>
+                <br>
+                <br>
+                <a href="index.html">Voltar</a>
+            </section>
+        </main>
+    </body>
+</html>
