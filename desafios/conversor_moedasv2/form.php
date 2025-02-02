49d41920f0982ab498847822d70b36078b4e7e58
Lucas Galindo Martins <165849134+LucasGalindoMartins@users.noreply.github.com>
2025-02-02 12:36:00 -0300

    Conversor de Moedas usando API (incompleto)

---
+++ desafios/conversor_moedasv2/form.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt=br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Resultado</title>
+        <link rel="stylesheet" href="styles/style.css">
+    </head>
+    <body>
+        <main>
+            <section>
+                <h1>Conversor de Moedas v1.0</h1>
+
+                <?php 
+                    $saldo = $_GET["saldo"];
+                    $dolar = 5.22;
+                    $res = number_format($saldo/$dolar, 2, ',');
+
+                    echo "Seus <strong>R$ $saldo</strong> equivalem a <strong>US$ $res</strong>. <br> <br>";
+                    echo "Cotação obtida diretamente do site do <strong>Banco Central do Brasil</strong>"
+                ?>
+                <br>
+                <br>
+                <a href="index.html">Voltar</a>
+            </section>
+        </main>
+    </body>
+</html>
